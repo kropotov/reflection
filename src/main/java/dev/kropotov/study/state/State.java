f3e18cb70b76b3ffd28359ecf56b9ceca7356c67
ANKropotov <ANKropotov@inno.tech>
2024-06-14 17:30:01 +0500

    + State: List -> Map

--- src/main/java/dev/kropotov/study/state/State.java
+++ src/main/java/dev/kropotov/study/state/State.java
@@ -3,20 +3,20 @@ package dev.kropotov.study.state;
 import lombok.EqualsAndHashCode;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @EqualsAndHashCode
 public class State {
     private final Object obj;
-    private final List<Object> fields = new ArrayList<>();
+    private final Map<Field, Object> fieldValues = new HashMap<>();
 
     public State(Object obj) {
         this.obj = obj;
         for (Field field : obj.getClass().getDeclaredFields()) {
             field.setAccessible(true);
             try {
-                fields.add(field.get(obj));
+                fieldValues.put(field, field.get(obj));
             } catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
